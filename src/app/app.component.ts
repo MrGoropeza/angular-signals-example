07a46f33fe2a6b638c199a4bcff23330b82e581b
MrGoropeza <goropeza8@gmail.com>
2023-06-20 16:15:46 -0300

    feat: app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { MenuItem } from "primeng/api";
+import { MenubarModule } from "primeng/menubar";
+import { PanelModule } from "primeng/panel";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [RouterModule],
-  template: ` <router-outlet></router-outlet> `,
+  styleUrls: ["./app.component.scss"],
+  imports: [RouterModule, MenubarModule, PanelModule],
+  template: `
+    <main class="app-container">
+      <p-menubar [model]="menuItems">
+        <ng-template pTemplate="end">
+          <img src="assets/siltium-logo.svg" height="30" />
+        </ng-template>
+      </p-menubar>
+      <p-panel class="app-panel">
+        <router-outlet></router-outlet>
+      </p-panel>
+    </main>
+  `,
 })
-export class AppComponent {}
+export class AppComponent {
+  menuItems: MenuItem[] = [
+    {
+      label: "Signals vs RxJS",
+      routerLink: "",
+    },
+  ];
+}
